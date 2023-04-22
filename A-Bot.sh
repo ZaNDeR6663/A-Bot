c68b0261f694c2126c24cf27864cf58e4fce6a7e
Andrey Plotnikov <59998826+ZaNDeR6663@users.noreply.github.com>
2023-04-22 16:12:48 +0300

    Add files via upload

---
+++ A-Bot.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+blue='\033[0;36m'
+yellow='\033[0;33m'
+normal='\033[0m'
+clear
+echo "${yellow}Updating package...${normal}"
+apt-get update
+apt-get upgrade
+clear
+
+echo "${yellow}Installing screen...${normal}"
+apt-get install git wget curl screen
+clear
+
+echo "${yellow}Installing DCA bot...${normal}"
+if [ -f "$PWD/A-Bot" ]; then
+    clear
+    echo "${blue}A-Bot is already installed!${normal}"
+else
+    echo "$(wget 'abot-trade.ru/A-Bot')"
+    echo "$(chmod 755 'A-Bot')"
+    if ! screen -list | grep -q "abot"; then
+        echo "$(screen -S abot)"
+        echo "$(screen -x)"
+    fi
+fi
